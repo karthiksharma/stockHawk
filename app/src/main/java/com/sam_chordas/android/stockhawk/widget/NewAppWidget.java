185fa68a5db2cd5831ad80fa81c7b0c491bf4e13
Karthik Sharma <karthsharma@gmail.com>
2016-06-19 13:44:21 +0530

    Final p3 solution v01

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/NewAppWidget.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/NewAppWidget.java
@@ -1,13 +1,10 @@
 package com.sam_chordas.android.stockhawk.widget;
 
-import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
 
@@ -46,12 +43,14 @@ public class NewAppWidget extends AppWidgetProvider {
             views.setOnClickPendingIntent(R.id.widget_text, pendingIntent);
             views.setRemoteAdapter(R.id.widget_list,
                     new Intent(context, WidgetStockRemoteViewsService.class));
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+    /*        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
                 setRemoteAdapter(context, views);
             } else {
                 setRemoteAdapterV11(context, views);
             }
-
+*/
+            views.setRemoteAdapter(R.id.widget_list,
+                    new Intent(context, WidgetStockRemoteViewsService.class));
             // Set up collection items
             Intent clickIntentTemplate = new Intent(context, MyStocksActivity.class);
             PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
@@ -83,29 +82,29 @@ public class NewAppWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 */
-
-    /**
+/*
+    *//**
      * Sets the remote adapter used to fill in the list items
      *
      * @param context the context used to launch the intent
      * @param views RemoteViews to set the RemoteAdapter
-     */
+     *//*
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
         views.setRemoteAdapter(R.id.widget_list,
                 new Intent(context, WidgetStockRemoteViewsService.class));
     }
 
-    /**
+    *//**
      * Sets the remote adapter used to fill in the list items
      *
      * @param context the context to launch the intent
      * @param views RemoteViews to set the RemoteAdapter
-     */
+     *//*
     @SuppressWarnings("deprecation")
     private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
         views.setRemoteAdapter(0, R.id.widget_list,
                 new Intent(context, WidgetStockRemoteViewsService.class));
-    }
+    }*/
 }
 
